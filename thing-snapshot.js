1021561a50723a9e9e50907e2dcf91580b8926ad
root <you@example.com>
2022-09-10 21:39:46 -0700

    Develop different stack poll intervals.

--- thing-snapshot.js
+++ thing-snapshot.js
@@ -10,7 +10,7 @@ const {
 const axios = require("axios");
 
 const datagrams = [{}];
-const ignoreRemotePollInterval = false;
+const ignoreRemotePollInterval = true;
 // 26 September 2021
 console.log("thing-snapshot 1.0.3 10 September 2022");
 
@@ -23,24 +23,41 @@ var station = process.env.STATION;
 var from = process.env.FROM;
 var snapshotPaths = process.env.SNAPSHOTS.split(" ");
 
+const snapshotFile = "/tmp/snapshot.json";
+
 the_interval = interval_milliseconds;
 
-interval = setInterval(function () {
-  const q = handleLine(null);
+var currentStackPollInterval = the_interval;
+
+let interval = setInterval(function () {
+  const q = handleLine(null).then((arr) => {
+    //        var datagram = JSON.stringify(arr);
+
+    var snapshot = JSON.stringify({
+      ...arr,
+      thingReport: { snapshot: arr.agent_input },
+    });
+
+    filePost(snapshot);
+  });
 
   currentPollInterval = the_interval;
+  console.log("filePost currentPollInterval", currentPollInterval);
 }, the_interval);
 
-function handleLine(line) {
-  /*
-        REFERENCE
-        $datagram = [
-            "to" => "null" . $this->mail_postfix,
-            "from" => "job",
-            "subject" => "s/ job stack",
-        ];
-  */
+the_interval = interval_milliseconds;
 
+let stackInterval = setInterval(function () {
+  var p = currentStackPollInterval;
+  const q = handleLine(null).then((arr) => {
+    var datagram = JSON.stringify(arr);
+
+    stackPost(datagram).then((p) => {
+    });
+  });
+}, currentStackPollInterval);
+
+function handleLine(line) {
   //  var to = channel;
 
   var to = "snapshot";
@@ -49,16 +66,11 @@ function handleLine(line) {
   const subject = line;
   var agent_input = "snapshot";
 
-  //  match = false;
-
-  console.log(subject);
-
   // Otherwise this is a different datagram.
   // Save it in local memory cache.
 
   // https://dev.to/aminnairi/read-files-using-promises-in-node-js-1mg6
 
-  //console.log("SUBJECT", subject);
   const timestamp = new Date();
   const utc = timestamp.toUTCString();
   try {
@@ -72,11 +84,10 @@ function handleLine(line) {
         });
     });
 
-    Promise.all(p)
+    return Promise.all(p)
       .then((promises) => {
         const data2 = promises[0];
 
-        //        agent_input = data;
         var c = {};
         var parsed = promises.map((data) => {
           try {
@@ -88,13 +99,8 @@ function handleLine(line) {
           }
         });
 
-        //const p = JSON.parse(data2);
         const p = c;
-        //        parsed = { ...parsed, refreshedAt: timestamp };
-        //        parsed = { ...parsed, refreshedAt: timestamp };
         parsed = { ...p, refreshedAt: timestamp };
-
-        console.log("parsed", parsed);
 
         var arr = {
           from: from,
@@ -104,99 +110,8 @@ function handleLine(line) {
           precedence: "routine",
           interval: currentPollInterval,
         };
-        console.log("Prepared snapshot datagram");
-        console.log(arr);
-        var datagram = JSON.stringify(arr);
 
-        var snapshot = JSON.stringify({
-          ...arr,
-          thingReport: { snapshot: parsed },
-        });
-
-        fs.writeFile("/tmp/snapshot.json", snapshot, "utf8", function (err) {
-          if (err) return console.log(err);
-          console.log("Wrote snapshot file.");
-        });
-
-        if (transport === "apache") {
-          axios
-            .post(http_transport, datagram, {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            })
-            .then((result) => {
-              const thing_report = result.data.thingReport;
-
-              const requestedPollInterval =
-                thing_report && thing_report.requested_poll_interval;
-              console.log("thing_report", thing_report);
-              console.log("requested_poll_interval", requestedPollInterval);
-
-              if (
-                parseFloat(requestedPollInterval) !==
-                parseFloat(currentPollInterval)
-              ) {
-                if (requestedPollInterval === "x") {
-                } else if (requestedPollInterval === "z") {
-                } else {
-                  if (ignoreRemotePollInterval) {
-                    var i = parseFloat(requestedPollInterval);
-                    clearInterval(interval);
-                    interval = setInterval(function () {
-                      // do your stuff here
-                      console.log("hosts", hosts);
-                      hosts.map((h) => {
-                        var host = h;
-                        handleLine(null);
-                      });
-                      currentPollInterval = i;
-                    }, i);
-                  }
-                }
-              }
-
-              // Create a fallback message.
-              // Which says 'sms'.
-              sms = "sms";
-              message = "sms";
-
-              try {
-                //      var thing_report = JSON.parse(job.response);
-                var sms = thing_report.sms;
-                var message = thing_report.message;
-                //var agent = thing_report.agent;
-                //var uuid = thing_report.thing.uuid;
-              } catch (e) {
-                console.log(e);
-
-                var sms = "quiet";
-                var message = "Quietness. Just quietness.";
-              }
-
-              //    const image_url = thing_report && thing_report.link ? thing_report.link + '.png' : null
-
-              const image_url =
-                thing_report && thing_report.image_url
-                  ? thing_report.image_url
-                  : null;
-
-              if (sms !== null) {
-                if (image_url === null) {
-                  console.log(sms);
-                  //        discordMessage.channel.send(sms);
-                } else {
-                  console.log(sms);
-                  console.log("image(s) available");
-                  //        discordMessage.channel.send(sms, { files: [image_url] });
-                }
-              }
-            })
-            .catch((error) => {
-              console.log("POST ERROR", error);
-              Promise.resolve("ignore");
-            });
-        }
+        return arr;
       })
       .catch((error) => {
         console.error("Read error", error);
@@ -207,6 +122,106 @@ function handleLine(line) {
         Promise.resolve("ignore");
       });
   } catch (err) {
-    console.log("Promise all", err);
+    console.log("Promise all error", err);
+  }
+}
+
+function filePost(snapshot) {
+  fs.writeFile(snapshotFile, snapshot, "utf8", function (err) {
+    if (err) return console.log(err);
+    console.log("filePost wrote snapshot " + snapshotFile);
+  });
+}
+
+function stackPost(datagram) {
+  if (transport === "apache") {
+    var i = currentStackPollInterval;
+    return axios
+      .post(http_transport, datagram, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((result) => {
+        console.log("stackPost " + http_transport);
+        const thing_report = result.data.thingReport;
+
+        const requestedPollInterval =
+          thing_report && thing_report.requested_poll_interval;
+        console.log("requested_poll_interval", requestedPollInterval);
+
+        if (
+          parseFloat(requestedPollInterval) !== parseFloat(currentStackPollInterval)
+        ) {
+          if (requestedPollInterval === "x") {
+          } else if (requestedPollInterval === "z") {
+          } else {
+            var i = parseFloat(requestedPollInterval);
+
+            if (ignoreRemotePollInterval) {
+              //              var i = parseFloat(requestedPollInterval);
+              clearInterval(stackInterval);
+              console.log("stack set interval i", i);
+                  currentStackPollInterval = i;
+
+              stackInterval = setInterval(function () {
+                // do your stuff here
+
+                handleLine(null).then((arr) => {
+                  var datagram = JSON.stringify(arr);
+
+                  stackPost(datagram).then((p) => {
+console.log("stackPost interval ", currentStackPollInterval);
+});
+
+                });
+
+              }, i);
+            }
+          }
+        }
+
+        // Create a fallback message.
+        // Which says 'sms'.
+        sms = "sms";
+        message = "sms";
+
+        try {
+          //      var thing_report = JSON.parse(job.response);
+          var sms = thing_report.sms;
+          var message = thing_report.message;
+          //var agent = thing_report.agent;
+          //var uuid = thing_report.thing.uuid;
+        } catch (e) {
+          console.error("message and sms not found", e);
+
+          var sms = "quiet";
+          var message = "Quietness. Just quietness.";
+        }
+
+        //    const image_url = thing_report && thing_report.link ? thing_report.link + '.png' : null
+
+        const image_url =
+          thing_report && thing_report.image_url
+            ? thing_report.image_url
+            : null;
+
+        if (sms !== null) {
+          if (image_url === null) {
+            console.log("sms", sms);
+            //        discordMessage.channel.send(sms);
+          } else {
+            console.log("sms", sms);
+            console.log("image(s) available");
+            //        discordMessage.channel.send(sms, { files: [image_url] });
+          }
+        }
+
+        return i;
+      })
+      .catch((error) => {
+        console.log("POST ERROR", error);
+        Promise.resolve("ignore");
+      });
   }
 }
